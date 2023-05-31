828237e1f8a0916ef448fe8ad812405c672c494e
Roman Khan Rajib <rajibkn345@gmail.com>
2023-06-01 01:22:31 +0600

    customize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,9 +42,9 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Raj &nbsp;
+            RKR &nbsp;
           </p>
         </Link>
 

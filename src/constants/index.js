cb79967ca167e476f868416775fd9f6f101a9cad
Roman Khan Rajib <rajibkn345@gmail.com>
2023-05-31 17:44:22 +0600

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,6 +24,14 @@ import {
   tripguide,
   threejs,
 } from "../assets";
+import everexpert from "../assets/company/everexpert.png";
+import paragon from "../assets/paragon.png";
+import bazar from "../assets/bazar365.png";
+import prokitibilash from "../assets/prokitibilash.png";
+import robo from "../assets/robo-friends.png";
+import trillo from "../assets/trillo-app.png";
+import face from "../assets/face-app.png";
+import beauty from "../assets/beautyshop.png";
 
 export const navLinks = [
   {
@@ -42,7 +50,7 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Front-end Developer",
     icon: web,
   },
   {
@@ -54,7 +62,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Programming Instructor",
     icon: creator,
   },
 ];
@@ -117,36 +125,10 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
+    company_name: "Glam World It",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "March 2021 - April 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -156,10 +138,10 @@ const experiences = [
   },
   {
     title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
+    company_name: "EverExpert Info Systems Ltd.",
+    icon: everexpert,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "May 2022 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -172,56 +154,54 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "It is always a great pleasure working with Rajib. He understands my requirements and how to help me well to accomplish my goals in my code. He explains things very well and is always there, I am happy to be working with him",
+    name: "Nick",
+    designation: "Backend Developer",
+    company: "Fiverr",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
+      "Rajib is very kind, patient and helpful. I was having some issues with my application and he was determined to help solve everything I needed help with. I am happy to work with him, please reach out to him for help !",
+    name: "Roland Green",
     designation: "COO",
-    company: "DEF Corp",
+    company: "Fiverr",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+      "Very much worth the money. The seller is very easy to reach, has great availability and went beyond of what was originally requested to serve my needs. I will continue my work with the seller in the future!",
+    name: "Maxnols",
     designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    company: "Fiverr",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
-    description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    name: "Paragon Food",
+    description: "An ecommerce application for food products",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "nodejs",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "ant design",
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
+    image: paragon,
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    name: "Bazar365",
+    description: "Web application for ecommerce bussiness for grocery items",
     tags: [
       {
         name: "react",
@@ -232,24 +212,28 @@ const projects = [
         color: "green-text-gradient",
       },
       {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
         name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "nest.js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: bazar,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "prokitibilash",
+    description:
+      "A comprehensive travel booking platform that allows users to tours and travels",
+    tags: [
+      {
+        name: "js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
         color: "green-text-gradient",
       },
       {
@@ -257,7 +241,64 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
+    image: prokitibilash,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "face detection app",
+    description: "A full stack applicaton of the face detection",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: face,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Robo friends app",
+    description:
+      "A react application that provides the ability to search robot and fully responsive features",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: robo,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Beauty shop",
+    description: "A react application that is fully responsive beauty shop",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "green-text-gradient",
+      },
+    ],
+    image: beauty,
     source_code_link: "https://github.com/",
   },
 ];
